45d00d3157d2827e5d523f9f25d04780feeb7c9e
FzrFernando <fernandozamorarico@gmail.com>
2021-03-10 20:02:15 +0100

    Main Linea completado

--- src/ejerlinea/LineaMain.java
+++ src/ejerlinea/LineaMain.java
@@ -28,6 +28,55 @@ public class LineaMain {
 		
 		Linea l1 = new Linea(p1,p2);
 		
+		char opc=' ';
+		while (opc != '3') {
+			System.out.println("1.- ¿Hacia dónde te quieres desplazar?(A-Arriba, B-ABajo, I-Izquierda, D-Derecha)");
+			System.out.println("2.- Mostrar línea");
+			System.out.println("3.- Salir");
+			System.out.println("Introduce la opción");
+			opc =teclado.nextLine().charAt(0);
+			
+			switch (opc) {
+			case '1':
+				System.out.println("Elige un movimiento (A-Arriba, B-ABajo, I-Izquierda, D-Derecha)");
+				char movimiento = teclado.nextLine().charAt(0);
+				switch (movimiento) {
+				case 'A':
+					System.out.println("Introduce cuantas posiciones quieres moverte");
+					cant = Double.parseDouble(teclado.nextLine());
+					l1.moverArriba(cant);
+					System.out.println("Se ha movido " + cant + " posiciones hacia arriba");
+					break;
+				case 'B':
+					System.out.println("Introduce cuantas posiciones quieres moverte");
+					cant = Double.parseDouble(teclado.nextLine());
+					l1.moverAbajo(cant);
+					System.out.println("Se ha movido " + cant + " posiciones hacia abajo");
+					break;
+				case 'I':
+					System.out.println("Introduce cuantas posiciones quieres moverte");
+					cant = Double.parseDouble(teclado.nextLine());
+					l1.moverIzquierda(cant);
+					System.out.println("Se ha movido " + cant + " posiciones hacia la izquierda");
+					break;
+				case 'D':
+					System.out.println("Introduce cuantas posiciones quieres moverte");
+					cant = Double.parseDouble(teclado.nextLine());
+					l1.moverDerecha(cant);
+					System.out.println("Se ha movido " + cant + " posiciones hacia la derecha");
+					break;
+				}
+				break;
+			case '2':
+				System.out.println(l1);
+				break;
+			case '3':
+				System.out.println("Adiós");
+				break;
+			default:
+				System.out.println("Opción no permitida");
+			}
+		}
 		
 	}
 
