75ce290e490b1f6e3d3a09b97ea44afe9f4952bb
Fernando <fernandozamorarico@gmail.com>
2021-03-11 09:49:11 +0100

    clase producto

---
+++ src/ejerproducto/Producto.java
@@ -0,0 +1,88 @@
+package ejerproducto;
+
+public class Producto {
+
+	private static final double IVA = 0.2;
+	private int codigo = 0;
+	private String descrip;
+	private double precio;
+	private static int codigosig = 1;
+	
+	public Producto(double precio, String descrip) {
+		super();
+		this.descrip = descrip;
+		this.precio = precio;
+		this.codigo = codigosig;
+		codigosig++;
+	}
+
+	public String getDescrip() {
+		return descrip;
+	}
+
+	public void setDescrip(String descrip) {
+		this.descrip = descrip;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public double precioConIva() {
+		this.precio =precio + precio * IVA;
+		return precio;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		result = prime * result + ((descrip == null) ? 0 : descrip.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(precio);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		if (codigo != other.codigo)
+			return false;
+		if (descrip == null) {
+			if (other.descrip != null)
+				return false;
+		} else if (!descrip.equals(other.descrip))
+			return false;
+		if (Double.doubleToLongBits(precio) != Double.doubleToLongBits(other.precio))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descrip=" + descrip + ", precio=" + precio + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/ejerproducto/ProductoMain.java
@@ -0,0 +1,14 @@
+package ejerproducto;
+
+public class ProductoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Producto p1 = new Producto (2.4,"galletas");
+		p1.precioConIva();
+		p1.toString();
+		System.out.println(p1);
+	}
+
+}
