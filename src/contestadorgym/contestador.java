27f4f1a8efcfa03edb177dcf64484ea17339553d
FzrFernando <fernandozamorarico@gmail.com>
2021-03-17 21:32:19 +0100

    Avanzando en el constestador

--- src/contestadorgym/contestador.java
+++ src/contestadorgym/contestador.java
@@ -7,16 +7,50 @@ public class contestador {
 	
 	public contestador() {
 		super();
-		this.mensaje1 = "";
-		this.mensaje2 = "";
-		this.mensaje3 = "";
+		this.mensaje1 = null;
+		this.mensaje2 = null;
+		this.mensaje3 = null;
+	}
+
+	public String getMensaje1() {
+		return mensaje1;
+	}
+
+	public void setMensaje1(String mensaje1) {
+		this.mensaje1 = mensaje1;
+	}
+
+	public String getMensaje2() {
+		return mensaje2;
+	}
+
+	public void setMensaje2(String mensaje2) {
+		this.mensaje2 = mensaje2;
+	}
+
+	public String getMensaje3() {
+		return mensaje3;
+	}
+
+	public void setMensaje3(String mensaje3) {
+		this.mensaje3 = mensaje3;
 	}
 	
+	public void borrarMensaje1() {
+		this.mensaje1 = null;
+	}
 	
+	public void borrarMensaje2() {
+		this.mensaje2 = null;
+	}
 	
+	public void borrarMensaje3() {
+		this.mensaje3 = null;
+	}
 	
-	
-	
+	public void anadirMensaje() {
+		if (this.getMensaje1())
+	}
 	
 	
 	
