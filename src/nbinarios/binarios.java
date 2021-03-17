9eb50d031f4b88040f3a49f06c6adb7509a2a061
Fernando <fernandozamorarico@gmail.com>
2021-03-17 08:48:38 +0100

    Pasar de binario a decimal

--- src/nbinarios/binarios.java
+++ src/nbinarios/binarios.java
@@ -13,12 +13,21 @@ public class binarios {
 	public static int calcularBinario(String binario) {
 		StringBuilder numero = new StringBuilder(binario);
 		numero.reverse();
-		int result = 0;
+		int cont = 0;
 		
-		for (int i=0; i<numero.length();i++) {
-			result = result + Integer.parseInt(String.valueOf(numero.charAt(i)));
+		for(int i=0; i < numero.length(); i++) {
+			if (numero.charAt(i) == '1') {
+				cont = (int) (cont + Math.pow(2,i));;
+			}
 		}
-		return result;
+		return cont;
+		
 	}
 
 }
+//int result = 0;
+//
+//for (int i=0; i<numero.length();i++) {
+//	result = result + Integer.parseInt(String.valueOf(numero.charAt(i)));
+//}
+//return result;
