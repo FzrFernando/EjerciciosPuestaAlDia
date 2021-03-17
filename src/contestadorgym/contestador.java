69b278f8595ef2b4bcbe814930a322e6609dfc4a
Fernando <fernandozamorarico@gmail.com>
2021-03-17 09:58:06 +0100

    Paquete contestador

---
+++ src/contestadorgym/contestador.java
@@ -0,0 +1,25 @@
+package contestadorgym;
+
+public class contestador {
+	private String mensaje1;
+	private String mensaje2;
+	private String mensaje3;
+	
+	public contestador() {
+		super();
+		this.mensaje1 = "";
+		this.mensaje2 = "";
+		this.mensaje3 = "";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
