3b65a02c67758adaf3e61345fbaed2779b89d501
Fernando <fernandozamorarico@gmail.com>
2021-03-18 09:40:56 +0100

    Contestador terminado

--- src/contestadorgym/contestador.java
+++ src/contestadorgym/contestador.java
@@ -7,29 +7,39 @@ public class contestador {
 	
 	public contestador() {
 		super();
-		this.mensaje1 = null;
-		this.mensaje2 = null;
-		this.mensaje3 = null;
+		this.mensaje1 = "";
+		this.mensaje2 = "";
+		this.mensaje3 = "";
 	}
 
-	public String getMensaje1() {
-		return mensaje1;
+	public String getMensaje1() throws Exception {
+		if (!this.mensaje1.equals("")) {
+			return mensaje1;
+		}
+		throw new Exception ("El mensaje 1 no existe");
+
 	}
 
 	public void setMensaje1(String mensaje1) {
 		this.mensaje1 = mensaje1;
 	}
 
-	public String getMensaje2() {
-		return mensaje2;
+	public String getMensaje2() throws Exception {
+		if (!this.mensaje2.equals("")) {
+			return mensaje2;
+		}
+		throw new Exception ("El mensaje 2 no existe");
 	}
 
 	public void setMensaje2(String mensaje2) {
 		this.mensaje2 = mensaje2;
 	}
 
-	public String getMensaje3() {
-		return mensaje3;
+	public String getMensaje3() throws Exception{
+		if (!this.mensaje3.equals("")) {
+			return mensaje3;
+		}
+		throw new Exception ("El mensaje 3 no existe");
 	}
 
 	public void setMensaje3(String mensaje3) {
@@ -37,23 +47,94 @@ public class contestador {
 	}
 	
 	public void borrarMensaje1() {
-		this.mensaje1 = null;
+		this.mensaje1 = "";
 	}
 	
 	public void borrarMensaje2() {
-		this.mensaje2 = null;
+		this.mensaje2 = "";
 	}
 	
 	public void borrarMensaje3() {
-		this.mensaje3 = null;
+		this.mensaje3 = "";
 	}
 	
-	public void anadirMensaje() {
-		if (this.getMensaje1())
+	public void anadirMensaje(String texto) throws Exception {
+		if (this.getMensaje1().equals("")) {
+			this.setMensaje1(texto);
+		}
+		else if (this.getMensaje2().equals("")) {
+			this.setMensaje2(texto);
+			
+		}
+		else if (this.getMensaje3().equals("")) {
+			this.setMensaje3(texto);
+		}
+		else {
+			throw new Exception ("“Buzón completo");
+		}
 	}
 	
+	public void borrarTodosLosMensajes() {
+		this.setMensaje1("");
+		this.setMensaje2("");
+		this.setMensaje3("");
+	}
 	
-	
+	public int contarMensajes() throws Exception {
+		int cont = 0;
+		if (!this.getMensaje1().equals("")) {
+			cont++;
+		}
+		if (!this.getMensaje2().equals("")) {
+			cont++;
+		}
+		if (!this.getMensaje3().equals("")) {
+			cont++;
+		}
+		return cont;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((mensaje1 == null) ? 0 : mensaje1.hashCode());
+		result = prime * result + ((mensaje2 == null) ? 0 : mensaje2.hashCode());
+		result = prime * result + ((mensaje3 == null) ? 0 : mensaje3.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		contestador other = (contestador) obj;
+		if (mensaje1 == null) {
+			if (other.mensaje1 != null)
+				return false;
+		} else if (!mensaje1.equals(other.mensaje1))
+			return false;
+		if (mensaje2 == null) {
+			if (other.mensaje2 != null)
+				return false;
+		} else if (!mensaje2.equals(other.mensaje2))
+			return false;
+		if (mensaje3 == null) {
+			if (other.mensaje3 != null)
+				return false;
+		} else if (!mensaje3.equals(other.mensaje3))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "contestador [mensaje1=" + mensaje1 + ", mensaje2=" + mensaje2 + ", mensaje3=" + mensaje3 + "]";
+	}
 	
 	
 }
--- src/contestadorgym/contestadorMain.java
+++ src/contestadorgym/contestadorMain.java
@@ -1,10 +1,144 @@
 package contestadorgym;
 
+import java.util.Scanner;
+
 public class contestadorMain {
 
+	static Scanner teclado = new Scanner (System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String texto;
+		
+		contestador c1 = new contestador();
+		
+		char opc = ' ';
+		while (opc != '5') {
+			System.out.println("1. Consultar número de mensajes");
+			System.out.println("2. Consultar mensaje");
+			System.out.println("3. Añadir mensaje");
+			System.out.println("4. Borrar todos los mensajes");
+			System.out.println("5. Salir");
+			opc = teclado.nextLine().charAt(0);
+			
+			switch(opc) {
+			case '1':
+				try {
+					c1.contarMensajes();
+					break;
+				}
+				catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			case '2':
+				char msg = ' ';
+				while (msg != '3') {
+					System.out.println("1. Mensaje número 1");
+					System.out.println("2. Mensaje número 2");
+					System.out.println("3. Mensaje número 3");
+					System.out.println("4. Salir");
+					msg = teclado.nextLine().charAt(0);
+					
+					switch(msg) {
+					case '1':
+						char msg1 = ' ';
+						//while (msg1 != '2') {
+							System.out.println("1. Oír");
+							System.out.println("2. Borrar");
+							msg1 = teclado.nextLine().charAt(0);
+							
+							switch(msg1) {
+							case '1':
+								try {
+									c1.getMensaje1();
+									break;
+								}
+								catch (Exception e){
+									System.out.println(e.getMessage());
+								}
+								break;
+							case '2':
+								c1.borrarMensaje1();
+								break;
+							}
+						//}
+						break;
+					case '2':
+						char msg2 = ' ';
+						//while (msg2 != '2') {
+							System.out.println("1. Oír");
+							System.out.println("2. Borrar");
+							msg2 = teclado.nextLine().charAt(0);
+							
+							switch(msg2) {
+							case '1':
+								try {
+									c1.getMensaje2();
+									
+								}
+								catch (Exception e){
+									System.out.println(e.getMessage());
+								}
+								break;
+							case '2':
+								c1.borrarMensaje2();
+								break;
+							}
+					//	}
+							break;
+					case '3':
+						char msg3 = ' ';
+						//while (msg3 != '3') {
+							System.out.println("1. Oír");
+							System.out.println("2. Borrar");
+							//System.out.println("3. Salir");
 
+							msg3 = teclado.nextLine().charAt(0);
+							
+							switch(msg3) {
+							case '1':
+								try {
+									c1.getMensaje3();
+									
+								}
+								catch (Exception e){
+									System.out.println(e.getMessage());
+								}
+								break;
+							case '2':
+								c1.borrarMensaje3();
+								break;
+							}
+						//}
+						break;
+					case '4':
+						System.out.println("Has terminado de leer los mensajes");
+					}
+					break;
+				}
+				break;
+			case '3':
+				try {
+					System.out.println("Añada el mensaje");
+					texto = teclado.nextLine();
+					c1.anadirMensaje(texto);
+					break;
+				}
+				catch (Exception e){
+					System.out.println(e.getMessage());
+				}
+				break;
+			case '4':
+				c1.borrarTodosLosMensajes();
+				break;
+			case '5':
+				System.out.println("Has finalizado con el contestador");
+				break;
+			default:
+				System.out.println("Introduzca una opción válida");
+			}
+
+		}
 	}
 
 }
